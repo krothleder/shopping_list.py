f9909883a0238e5b1b5d554060685ab23f005069
Michelle Choi <mcmiji@gmail.com>
2016-07-27 20:45:09 -0700

    calculator made

---
+++ calculator.py
@@ -0,0 +1,22 @@
+def add(num1, num2):
+	return num1 + num2
+
+def subtract(num1, num2):
+	return num1 - num2
+
+def multiply(num1, num2):
+	return num1 * num2
+
+def divide(num1, num2):
+	return num1 / num2
+
+def modulo(num1, num2):
+	return num1 % num2
+
+def power(num1, num2):
+	return num1 ** num2
+
+def square(num):
+	return num ** 2
+
+print power (3, add(2,3))
