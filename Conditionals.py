85a8b0e534be2d792cad66163688f2664cc226e9
Michelle Choi <mcmiji@gmail.com>
2016-07-27 20:50:54 -0700

    initial commit

---
+++ Conditionals.py
@@ -0,0 +1,5 @@
+num = int(raw_input("type the number eight"))
+if (num/ == 0):
+	print "the number 8 is even"
+else: 
+	print "8 is odd"
---
+++ conditionals.py
@@ -0,0 +1,18 @@
+# my_name = "Timea"
+# pair_name = "Marla"
+
+# if my_name > pair_name:
+# 	print "my name is greater!"
+
+# elif pair_name > my_name:
+# 	print "their name is greater!"
+
+# else:
+# 	print "our names are equal"
+
+
+date = 21
+if date >= 15:
+	print "We're halfway there!"
+if date <= 14:
+	print "The month is still young."
